4a981e9951ed758d1d92afe28564627d2381ba7f
Pruthvi Raj <pruthviraj.1rn16ec097@gmail.com>
2024-09-05 00:47:56 +0530

    ep2 done

--- App.js
+++ App.js
@@ -9,9 +9,12 @@
 //    </div> 
 // </div> 
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id:"parent"}, [
     React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm an H1 tag"), 
+        React.createElement("h1", {}, "This is namaste react"), 
         React.createElement("h2", {}, "I'm an h2 tag"),
     ]),
     React.createElement("div", {id: "child2"}, [
