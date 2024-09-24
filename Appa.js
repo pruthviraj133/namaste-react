7b7ff491143222d88cca6808cf84686a12b4dad4
Pruthvi Raj <pruthviraj.1rn16ec097@gmail.com>
2024-09-24 08:07:43 +0530

    24.9.24 upd

--- Appa.js
+++ Appa.js
@@ -1,36 +1,49 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
 /**
- * Head
- * - Title
+ * Header
+ * - logo
+ * - Nav Items
  * Body
- * - AppLayout
- * - Card
- * - Data
- * @returns 
+ * - Search
+ * - restuarant card container
+ * - cards
+ * Footer
+ * - copyright
+ * - links
+ * - address
+ * - contact
+ * 
  */
 
 const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
-                <img
-                    className="logo"
-                    src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg" 
-                />
+                <img 
+                className = "logo"
+                src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg" />
             </div>
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
                     <li>Cart</li>
                 </ul>
+                 
             </div>
         </div>
     );
 };
+
+// this object declared can be proivded as input to card as style
+// const styleCard = {
+//     backgroundColor: "#f0f0f0",
+// }
 
 const RestaurantCard = (props) => {
     const { resName, cuisine, rating, deltime } = props;
@@ -38,9 +51,9 @@ const RestaurantCard = (props) => {
     return (
         <div className="res-card" style={{ backgroundColor: "#f0f0f0"}}> 
             <img 
-                className="res-logo"
-                alt="res-logo" 
-                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"/>
+            className="res-logo"
+            alt="res-logo" 
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"/>
             <h3>{resName}</h3>
             <h4>{cuisine}</h4>
             <h4>{rating}</h4>
@@ -50,26 +63,32 @@ const RestaurantCard = (props) => {
 };
 
 const Body = () => {
-    return (
+    return(
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
+                {/* res-card */}
                 <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian" rating="4.5" deltime="36 mins"/>
-                <RestaurantCard resName="KFC" cuisine="Biryani, North Indian, Asian" rating="4.5" deltime="36 mins"/>
+                <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" rating="4.1" deltime="24 mins"/>
             </div>
 
         </div>
     );
 };
 
+
+
+
 const AppLayout = () => {
-    return(
-        <div className = "app">
+    return (
+        <div className="app">
             <Header />
             <Body />
         </div>
     );
 };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />)
+root.render(<AppLayout />);
+
