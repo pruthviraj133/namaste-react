7261e4c269119569f8ae5aaa00845f1213b445d0
Pruthvi Raj <pruthviraj.1rn16ec097@gmail.com>
2024-12-23 21:52:30 +0530

    ep 10 done

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,14 +6,14 @@ const RestaurantCard = (props) => {
     const { name, cuisines, avgRating, costForTwo, cloudinaryImageId, sla } = resData?.info;
     
     return (
-        <div className="res-card" style={{ backgroundColor: "#f0f0f0"}}> 
+        <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-200 hover:bg-gray-300"> 
             <img 
-            className="res-logo"
+            className="rounded-lg"
             alt="res-logo" 
             src={
                 CDN_URL + cloudinaryImageId
             }/>
-            <h3>{name}</h3>
+            <h3 className="font-bold py-2 text-lg">{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating}</h4>
             <h4>{costForTwo}</h4>
